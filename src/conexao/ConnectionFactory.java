363a02eac4694cfb87c773d144a4b0a0c598d6e5
Luiz Altimari <luizc.altimari@gmail.com>
2019-08-19 16:26:16 -0300

    conexão com o banco

--- src/conexao/ConnectionFactory.java
+++ src/conexao/ConnectionFactory.java
@@ -7,7 +7,7 @@ public class ConnectionFactory {
 
 	public Connection getConnection(){
 		try {
-			return DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "12345");
+			return DriverManager.getConnection("jdbc:mysql://localhost:3306/Trabalho02?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "12345");
 		}catch (Exception e) {
 			throw new RuntimeException(e);
 		}
